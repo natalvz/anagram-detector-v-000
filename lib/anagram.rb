ab72216e06a0d248320df9e6ba4026a26a11fbc6
Natalia Castro <nat.alvz@gmail.com>
2018-06-01 21:01:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,13 @@
 # Your code goes here!
 class Anagram
   attr_accessor :word
-  @@anagrams = []
+
   def initialize(word)
     @word = word
   end
   
   def match(array)
+    matches = []
     
   end
 end
